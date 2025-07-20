5de13619653019e54c118714747972f1196dd508
A.r.gh_94 <hopgpli@gmail.com>
2025-07-20 20:19:12 +0330

    Update main.py

--- Files/main.py
+++ Files/main.py
@@ -231,7 +231,8 @@ if __name__ == "__main__":
         "https://t.me/s/MTPro_XYZ",
         "https://t.me/s/vpns",
         "https://t.me/s/mtmvpn",
-        "https://t.me/s/asr_proxy"
+        "https://t.me/s/asr_proxy",
+        "https://t.me/s/proxyskyy"
     ]
     
     text_proxies = fetch_proxies_from_text_urls(text_urls)
